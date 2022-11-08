120d742a3cb93fc7922c2af241b115fa5c2552d0
jefflee1103 <jefflee1103@gmail.com>
2022-11-08 18:16:32 +0000

    Fixed codes for rblast

--- R/COVID_probes.R
+++ R/COVID_probes.R
@@ -5,7 +5,7 @@
 # Environment ---------------------------------------------------------------------------------
 
 library(tidyverse)
-source("./src/HCR_design.R")
+source("./R/HCR_design.R")
 
 
 # Nucleocapsid --------------------------------------------------------------------------------
@@ -14,20 +14,46 @@ source("./src/HCR_design.R")
 
 first <- read_delim(pipe("pbpaste"), col_names = "first", delim = "\t") 
 second <- read_delim(pipe("pbpaste"), col_names = "second", delim = "\t")
-SARS_N <- bind_cols(first, second)
-SARS_N 
-
-
-SARS_N <- SARS_N %>%
+probe_set <- bind_cols(first, second)
+probe_set 
+probe_set <- probe_set %>%
   mutate(first = str_sub(first, start = 1, end = 25),
          second = str_sub(second, start = 21, end = 45))
-SARS_N
+probe_set
 
-tmp <- attach_hcr_initiatior_standalone(SARS_N,
+output <- attach_hcr_initiatior_standalone(probe_set,
                                  "first",
                                  "second",
-                                 "B3",
+                                 "B1",
                                  "SARS2-N") %>%
   dplyr::select(5,6,7,8)
 
+output
+
+write_csv(output, "~/Desktop/SARS2-N_HCRB1_probes-to-order.csv")
+
+
+# ORF1a -------------------------------------------------------------------
+
+## Get pre-designed sequences from SS lab, remove pre-appended B1 hairpin and attach B# of my choice
+
+first <- read_delim(pipe("pbpaste"), col_names = "first", delim = "\t") 
+second <- read_delim(pipe("pbpaste"), col_names = "second", delim = "\t")
+probe_set <- bind_cols(first, second)
+probe_set 
+probe_set <- probe_set %>%
+  mutate(first = str_sub(first, start = 1, end = 25),
+         second = str_sub(second, start = 21, end = 45))
+probe_set
+
+output <- attach_hcr_initiatior_standalone(probe_set,
+                                           "first",
+                                           "second",
+                                           "B3",
+                                           "SARS2-orf1a") %>%
+  dplyr::select(5,6,7,8)
+
+output
+
+write_csv(output, "~/Desktop/SARS2-orf1a_HCRB3_probes-to-order.csv")
 
